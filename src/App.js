81fa1d5c3b9f55e6bc30498af5bdb4dab1413d15
Steven <code_sergeant@protonmail.com>
2019-02-12 18:26:06 -0800

    Setting up layout components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './layout/Header';
+import About from './layout/About';
+import Resume from './layout/Resume';
+import Work from './layout/Work';
+import Testimonials from './layout/Testimonials';
+import Contact from './layout/Contact';
 
 export default function App() {
   return (
     <div>
       <Header />
+      <About />
+      <Resume />
+      <Work />
+      <Testimonials />
+      <Contact />
     </div>
   );
 }
